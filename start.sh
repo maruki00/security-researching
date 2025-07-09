bb96e1a57af21954976b776953c341571aa36a71
Abdellah Oulahyane <aitbaha48@gmail.com>
2025-07-10 00:58:56 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,2 @@
-
 #!/bin/sh
-docker run --rm -v "$(pwd)/host:/host" -v "$(pwd)/logs:/logs" --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -d --name security-research -i  security-researsh:ubuntu23.04
-
+docker run --rm -it security-research-lab /bin/bash
